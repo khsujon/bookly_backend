c072db07dd11af7077b98df81e30ca7551532d1a
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-09 21:48:17 +0600

    Add query parameter example to FastAPI application

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ async def get_health():
     return {
     'status': 'ok'
     }
+#Path parameter example
 @app.get("/great/{name}")
 async def great(name: str) -> dict:
-    return {"message":f"Hello {name}!"}
+    return {"message":f"Hello {name}!"} # request send as http://127.0.0.1:8000/great/YourName
+#Query parameter example
+@app.get("/items/")
+async def get_item(name: str, price: float) -> dict:
+    return {"name": name, "price": price} # request send as http://127.0.0.1:8000/items/?name=Item1&price=10.5
