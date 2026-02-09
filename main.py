3d5e5a04c85a3d56a8b99bab66dcf93ae16f0727
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-09 21:53:37 +0600

    Add optional query parameter example to users endpoint in FastAPI application

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 
 app = FastAPI()
@@ -13,7 +14,15 @@ async def get_health():
 @app.get("/great/{name}")
 async def great(name: str) -> dict:
     return {"message":f"Hello {name}!"} # request send as http://127.0.0.1:8000/great/YourName
+
 #Query parameter example
 @app.get("/items/")
 async def get_item(name: str, price: float) -> dict:
     return {"name": name, "price": price} # request send as http://127.0.0.1:8000/items/?name=Item1&price=10.5
+
+#optional query parameter example
+@app.get("/users/")
+async def get_user(name: str, age: Optional[int] = 25) -> dict:
+    if age:
+        return {"name": name, "age": age}
+    return {"name": name} # request send as http://127.0.0.1:8000/users/?name=User1
