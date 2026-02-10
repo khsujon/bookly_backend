91271026dd761b7789c07d0db1563a15a481b659
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-10 12:20:38 +0600

    Rename items endpoint to book and add create_book endpoint for book creation

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def great(name: str) -> dict:
     return {"message":f"Hello {name}!"} # request send as http://127.0.0.1:8000/great/YourName
 
 #Query parameter example
-@app.get("/items/")
+@app.get("/book/")
 async def get_item(name: str, price: float) -> dict:
     return {"name": name, "price": price} # request send as http://127.0.0.1:8000/items/?name=Item1&price=10.5
 
@@ -26,3 +26,17 @@ async def get_user(name: str, age: Optional[int] = 25) -> dict:
     if age:
         return {"name": name, "age": age}
     return {"name": name} # request send as http://127.0.0.1:8000/users/?name=User1
+
+#defining a model for request body
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    price : float
+#Create a book using request body
+@app.post("/create")
+async def create_book(book:BookCreate):
+    return{
+    "title": book.title,
+    "author": book.author,
+    "price": book.price
+    }
