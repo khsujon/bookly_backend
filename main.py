dc790b0a43a48909b6e03f9850ed52607655865a
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-11 13:23:55 +0600

    Add book data to books_data.py

--- main.py
+++ main.py
@@ -7,38 +7,6 @@ from typing import List
 app = FastAPI()
 
 
-#book data
-books = [
-    {"id": 1,
-    "title": "The Great Gatsby",
-    "author": "F. Scott Fitzgerald",
-    "publisher": "Scribner",
-    "published_date": "1925-04-10",
-    "page_count": 218,
-    "language": "English",},
-    {"id": 2,
-    "title": "To Kill a Mockingbird",
-    "author": "Harper Lee",
-    "publisher": "J.B. Lippincott & Co.",
-    "published_date": "1960-07-11",
-    "page_count": 281,
-    "language": "English",},
-    {"id": 3,
-    "title": "1984",
-    "author": "George Orwell",
-    "publisher": "Secker & Warburg",
-    "published_date": "1949-06-08",
-    "page_count": 328,
-    "language": "English",},
-    {"id": 4,
-    "title": "Pride and Prejudice",
-    "author": "Jane Austen",
-    "publisher": "T. Egerton",
-    "published_date": "1813-01-28",
-    "page_count": 432,
-    "language": "English",},
-    
-]
 
 #response model
 class Book(BaseModel):
