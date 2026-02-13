8e55b78ad4fe9ef80b13c1dba68ec537695a52c6
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-13 21:24:42 +0600

    Add async lifespan context manager for FastAPI application startup and shutdown

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from .books.routes import book_router
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Perform any startup tasks here (e.g., connect to the database)
+    print("Starting up server...")
+    yield   
+    # Perform any shutdown tasks here (e.g., disconnect from the database)
+    print("Shutting down has been stopped")
+
 
 version = "v1"
 app = FastAPI(
     title="Bookly",
     description="A REST API for a book review application built with FastAPI",
     version=version,
+    lifespan=lifespan
 )
 app.include_router(book_router, prefix=f"/{version}/books", tags=["books"])
 @app.get("/")
--- src/db/main.py
+++ src/db/main.py
@@ -1,6 +1,8 @@
 from sqlmodel import create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
+
+
 # Create an asynchronous engine
 engine = AsyncEngine(
     create_engine(Config.DATABASE_URL, echo=True)
