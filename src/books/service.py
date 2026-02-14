a0421b74868d898bf3098e7adea56685939e460f
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-14 21:38:22 +0600

    Implement get_all_books method in BookService to retrieve and return all books ordered by creation date

--- src/books/service.py
+++ src/books/service.py
@@ -2,10 +2,13 @@
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 from . schemas import BookCreateModel, BookUpdate
-
+from sqlmodel import select, desc
+from .models import Book
 class BookService:
     async def get_all_books(self, session: AsyncSession):
-        pass
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.exec(statement)
+        return result.all()
     
     async def get_book(self, book_uid: str, session: AsyncSession):
         pass
