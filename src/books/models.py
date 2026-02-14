0afe3c2f01d2ed26e4f57b067e7302a1da73ac04
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-14 19:15:49 +0600

    Fix import statement for UUID in models.py

--- src/books/models.py
+++ src/books/models.py
@@ -1,6 +1,6 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
+from uuid import UUID
 
 class Book(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    title: str
+    
