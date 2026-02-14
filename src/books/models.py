e95978df326744098767b06379a5b117d0f9afd4
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-14 19:17:17 +0600

    Add missing newline at end of file in models.py

--- src/books/models.py
+++ src/books/models.py
@@ -3,4 +3,12 @@ from datetime import datetime
 from uuid import UUID
 
 class Book(SQLModel, table=True):
-    
+    id : UUID
+    title : str
+    author : str
+    publisher : str
+    publication_date : datetime
+    page_count : int
+    language : str
+    created_at : datetime
+    updated_at : datetime
