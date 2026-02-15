7e3c3f2c9f76b22999fa13aca458e80ca71496a9
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-15 20:41:07 +0600

    Refactor database engine initialization and session management in main.py

--- src/db/main.py
+++ src/db/main.py
@@ -1,10 +1,10 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
 from src.config import Config
-from src.books.models import Book
-
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 # Create an asynchronous engine
-engine = AsyncEngine(
+async_engine = AsyncEngine(
     create_engine(
         Config.DATABASE_URL, 
         echo=True
@@ -12,5 +12,15 @@ engine = AsyncEngine(
 )
 
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+        
+async def get_session()-> AsyncSession:
+    
+    Session = sessionmaker(
+        bind = async_engine,
+        class_= AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
