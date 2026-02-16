ca10d9a8965dd103c84012578237ef12e8bb0a55
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-16 20:45:07 +0600

    Refactor User model to define created_at and updated_at fields with SQLModel Field and Column

--- src/auth/models.py
+++ src/auth/models.py
@@ -19,5 +19,5 @@ class User(SQLModel, table=True):
     first_name: str
     last_name: str
     is_verified: bool
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now, nullable=False))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP(timezone=True), default=datetime.now, onupdate=datetime.utcnow, nullable=False))
