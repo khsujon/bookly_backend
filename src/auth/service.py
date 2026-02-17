7eb72bd49220c657104357fd7c0d01a8bfbe0b41
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-17 14:06:00 +0600

    Add create_user method to UserService for user creation

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,7 +1,7 @@
 from .models import User
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
-
+from .schemas import UserCreateModel
 
 
 
@@ -19,3 +19,14 @@ class UserService:
         user = await self.get_user_by_email(email, session)
         
         return True if user is not None else False
+    
+    
+    #create a new user
+    async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
+        user_data_dict = user_data.model_dump()
+        
+        new_user = User(**user_data_dict)
+        session.add(user_data)
+        await session.commit()
+        await session.refresh(user_data)
+        return user_data
