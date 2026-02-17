67bcebff2c95a7de9397a18254d261baa51430c1
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-17 13:02:45 +0600

    Refactor User model to use 'uid' as primary key and create initial migration for users table

--- src/auth/models.py
+++ src/auth/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
-    id : uuid.UUID = Field(
+    uid : uuid.UUID = Field(
         sa_column=Column(
             pg.UUID, 
             primary_key=True, 
@@ -25,4 +25,4 @@ class User(SQLModel, table=True):
     
     
     def __repr__(self):
-        return f'<User(id={self.id}, username="{self.username}", email="{self.email}")>' 
+        return f'<User(uid={self.uid}, username="{self.username}", email="{self.email}")>' 
