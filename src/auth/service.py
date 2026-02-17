0861c12a02586eae1f95bef776a699fc88e51ad0
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-17 13:56:56 +0600

    Implement get_user_by_email method in UserService for user retrieval by email

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,5 +1,6 @@
 from .models import User
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
 
 
 
@@ -8,5 +9,8 @@ class UserService:
     """Service class for user-related operations."""
     
     #check if user exists by email
-    async def get_user():
-        pass
+    async def get_user_by_email(self, email: str, session: AsyncSession):
+        statement = select(User).where(User.email == email)
+        result = await session.exec(statement)
+        user = result.first()
+        return user
