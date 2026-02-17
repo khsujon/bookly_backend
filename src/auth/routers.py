e695afac7e4164b9300efcc1a2a94e2de93a16b9
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-17 19:20:21 +0600

    Implement user account creation endpoint in auth router

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 from .schemas import UserCreateModel
+from .service import UserService
 
 
 auth_router = APIRouter()
+user_service = UserService()
 
 
  #create a user
  
 @auth_router.post("/signup")
-async def signup():
-    pass
+async def create_user_account(user_data: UserCreateModel):
+    email = user_data.email
+    
+    # Check if user already exists
+    user_exists = await user_service.user_exists(email)
+    
