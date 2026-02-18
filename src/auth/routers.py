986c8e48b44355c1b646f77a08be6fc79053236f
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-18 20:11:14 +0600

    Add login_user endpoint to auth router and remove unnecessary whitespace

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -10,7 +10,6 @@ user_service = UserService()
 
 
  #create a user
- 
 @auth_router.post("/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user_account(user_data: UserCreateModel, session : AsyncSession = Depends(get_session)):
     email = user_data.email
@@ -25,3 +24,9 @@ async def create_user_account(user_data: UserCreateModel, session : AsyncSession
     new_user = await user_service.create_user(user_data, session)
     
     return new_user
+
+
+#Login user
+@auth_router.post("/login")
+async def login_user():
+    pass
