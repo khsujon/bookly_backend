265311ad959ede35b9a76e132445064b0d1033cb
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-18 20:42:37 +0600

    Fix create_access_token to return integer timestamp for expiry

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -22,7 +22,7 @@ def verify_password(password: str, hash: str) -> bool:
 def create_access_token(user_data:dict, expiry:timedelta=None, refresh:bool=False):
     payload = {}
     payload['user']= user_data
-    payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(minutes=ACCESS_TOKEN_EXPIRY)),
+    payload['exp'] = int((datetime.now() + (expiry if expiry is not None else timedelta(minutes=ACCESS_TOKEN_EXPIRY))).timestamp())
     payload['jti'] = str(uuid.uuid4())
     payload['refresh'] = refresh
     
