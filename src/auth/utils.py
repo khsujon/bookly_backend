c66e090dc801f4160b45284fd17a8d45095de72a
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-18 20:03:07 +0600

    Refactor create_access_token function to include refresh parameter

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -18,11 +18,14 @@ def verify_password(password: str, hash: str) -> bool:
     return password_context.verify(password, hash)
 
 # CREATE ACCESS TOKEN
-def create_access_token(user_data:dict, expiry:timedelta=None):
+def create_access_token(user_data:dict, expiry:timedelta=None, refresh:bool=False):
     payload = {}
     payload['user']= user_data
     payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(minutes=ACCESS_TOKEN_EXPIRY)),
     payload['jti'] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+    
+    
     token = jwt.encode(
         payload=payload,
         key=Config.JWT_SECRET_KEY,
