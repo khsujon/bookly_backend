e6635a7974afa0ccb2446f578581ea280c8bcef1
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-18 19:48:54 +0600

    Refactor create_access_token function to accept optional expiry parameter and initialize payload

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -2,9 +2,11 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import jwt
 from src.config import Config
+import uuid
 
 password_context = CryptContext(schemes=["bcrypt"])
 
+ACCESS_TOKEN_EXPIRY = 60
 
 # Utility function to hash passwords
 def generate_password_hash(password: str) -> str:
@@ -16,12 +18,16 @@ def verify_password(password: str, hash: str) -> bool:
     return password_context.verify(password, hash)
 
 # CREATE ACCESS TOKEN
-def create_access_token(user_data:dict, expiry:timedelta):
+def create_access_token(user_data:dict, expiry:timedelta=None):
     payload = {}
-    
+    payload['user']= user_data
+    payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(minutes=ACCESS_TOKEN_EXPIRY)),
+    payload['jti'] = str(uuid.uuid4())
     token = jwt.encode(
         payload=payload,
         key=Config.JWT_SECRET_KEY,
         algorithm=Config.JWT_ALGORITHM,
         
     )
+    
+    return token
