f48c1a9046a7ee5fb2e74292f60601d7369f1d1c
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-19 17:31:47 +0600

    Add user authentication dependency to book routes

--- src/books/routes.py
+++ src/books/routes.py
@@ -15,18 +15,20 @@ access_token_bearer = AccessTokenBearer()
 #get all books
 @book_router.get("/", response_model= List[BookResponse])
 async def get_books(session: AsyncSession=Depends(get_session), user_details=Depends(access_token_bearer)):
+    
+    print(user_details)
     books = await book_service.get_all_books(session)
     return books
 
 #create a book
 @book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=BookResponse)
-async def create_book(book_data: BookCreateModel, session: AsyncSession=Depends(get_session)):
+async def create_book(book_data: BookCreateModel, session: AsyncSession=Depends(get_session), user_details=Depends(access_token_bearer)):
     new_book = await book_service.create_book(book_data, session)
     return new_book
 
 #get book by id
 @book_router.get("/{book_uid}", response_model=BookResponse)
-async def get_book(book_uid: str, session: AsyncSession=Depends(get_session)):
+async def get_book(book_uid: str, session: AsyncSession=Depends(get_session), user_details=Depends(access_token_bearer)):
     book = await book_service.get_book(book_uid, session)
     if book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book Not Found with id {book_uid}")
@@ -34,7 +36,7 @@ async def get_book(book_uid: str, session: AsyncSession=Depends(get_session)):
 
 #update a book
 @book_router.patch("/{book_uid}", response_model=BookResponse)
-async def update_book(book_uid: str, book_update_data: BookUpdate, session: AsyncSession=Depends(get_session)):
+async def update_book(book_uid: str, book_update_data: BookUpdate, session: AsyncSession=Depends(get_session), user_details=Depends(access_token_bearer)):
     updated_book =  await book_service.update_book(book_uid, book_update_data, session)
     if updated_book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book Not Found with id {book_uid}")
@@ -43,7 +45,7 @@ async def update_book(book_uid: str, book_update_data: BookUpdate, session: Asyn
 
 #delete a book
 @book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid: str, session: AsyncSession=Depends(get_session)):
+async def delete_book(book_uid: str, session: AsyncSession=Depends(get_session), user_details=Depends(access_token_bearer)):
     deleted_book = await book_service.delete_book(book_uid, session)
     if deleted_book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book Not Found with id {book_uid}")
