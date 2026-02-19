bab01e9db5d62993df84efc9f4268a060dc6e110
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-19 17:16:25 +0600

    Implement AccessTokenBearer class with custom authentication handling

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,5 +1,14 @@
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Request
+
 
 
 class AccessTokenBearer(HTTPBearer):
-    pass
+    
+    def __init__(self, auto_error: bool = True):
+        super(AccessTokenBearer, self).__init__(auto_error=auto_error)
+        
+    async def __call__(self, request: Request)->HTTPAuthorizationCredentials|None:
+        credential = await super().__call__(request)
+        
+        
