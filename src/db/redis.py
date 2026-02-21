228a8595d36211ab16fcb404e83f23fcf7b8bfa2
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-21 13:24:09 +0600

    Add function to check if a token's JTI is in the blocklist

--- src/db/redis.py
+++ src/db/redis.py
@@ -14,3 +14,8 @@ async def add_token_to_blocklist(jti: str) -> None:
     """Add a token's JTI to the blocklist"""
     await token_blocklist.set(name = jti,value = "",ex = JTI_EXPIRY)
     
+
+async def is_token_blocked(jti: str) -> bool:
+    """Check if a token's JTI is in the blocklist"""
+    jti= await token_blocklist.get(jti)
+    return jti is not None
