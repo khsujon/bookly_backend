09237fe85eecb2e53dc0f90a189039dadaead6a6
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-21 13:15:56 +0600

    Add Redis connection setup for token blocklist

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,2 +1,9 @@
 import aioredis
 from src.config import Config
+
+# Redis connection for token blocklist
+token_blocklist = aioredis.StrictRedis(
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT,
+    db=0,
+)
