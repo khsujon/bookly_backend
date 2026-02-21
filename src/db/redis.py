c91311e633101da9332a8c0dce3a8f121a28ca58
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-02-21 13:21:48 +0600

    Add function to add token's JTI to blocklist in Redis

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,9 +1,16 @@
 import aioredis
 from src.config import Config
 
+
+JTI_EXPIRY = 3600  # Time in seconds for how long the JTI should be stored in Redis (e.g., 1 hour)
 # Redis connection for token blocklist
 token_blocklist = aioredis.StrictRedis(
     host=Config.REDIS_HOST,
     port=Config.REDIS_PORT,
     db=0,
 )
+
+async def add_token_to_blocklist(jti: str) -> None:
+    """Add a token's JTI to the blocklist"""
+    await token_blocklist.set(name = jti,value = "",ex = JTI_EXPIRY)
+    
