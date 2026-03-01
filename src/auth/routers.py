fdcd708e80cc9965e2891d103a0077bb1e892aaf
Kamrul Hasan <kamrulhasan.km3@gmail.com>
2026-03-01 19:34:52 +0600

    Add role validation to get_current_user endpoint

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -97,7 +97,7 @@ async def get_new_access_token(token_details:dict=Depends(RefreshTokenBearer()))
     
 #get current user details
 @auth_router.get("/me")
-async def get_current_user(user = Depends(get_current_user)):
+async def get_current_user(user = Depends(get_current_user), _:bool=Depends(role_checker)):
     return user
     
     
